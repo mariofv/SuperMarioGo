ad3e3accd8470c5474a8adf8e84e1f228f777a8f
Mario <mario_bcn2@hotmail.es>
2018-05-15 02:14:11 +0200

    Started Main Menu

---
+++ Assets/MenuButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButton : MonoBehaviour {
+
+    public float rotationSpeed = 10f;
+
+    public GameObject model;
+    public Transform leftPoint;
+    public Transform rightPoint;
+
+    private GameObject buttonModel;
+
+    // Use this for initialization
+    void Start () {
+        buttonModel = Instantiate(model, gameObject.transform);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        buttonModel.transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+	}
+}
--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -12,6 +12,6 @@ public class NewBehaviourScript : MonoBehaviour {
 
     // Update is called once per frame
     void Update () {
-		
-	}
+        
+    }
 }
